659df1cd69d86cd8479a1f5b39c465d7acd89eab
OTSUKA Tatsuya <tatzyr@gmail.com>
2015-07-24 18:13:31 +0900

    Reduce CLI#run complexity

--- lib/alltweets/cli.rb
+++ lib/alltweets/cli.rb
@@ -4,45 +4,46 @@ require "trollop"
 module AllTweets
   class CLI
     def initialize
+      @screen_name, @opts = parse_args
       @settings = Settings.new
-    end
-
-    def collect(screen_name, include_retweets:)
-      collector = Collector.new(
+      @settings.get_access_token
+      @collector = Collector.new(
         consumer_key: @settings.consumer_key,
         consumer_secret: @settings.consumer_secret,
         access_token: @settings.access_token,
         access_token_secret: @settings.access_token_secret
       )
-
-      collector.get_all_tweets(screen_name, include_retweets: include_retweets)
+      @filename = filename
     end
 
     def run
-      @settings.get_access_token
+      puts "Saving #{@screen_name}'s all tweets to #{@filename}"
+      result = @collector.get_all_tweets(@screen_name, include_retweets: @opts[:retweets]).map(&:to_h)
 
+      if @opts[:json]
+        dump_data = Oj.dump(result, mode: :compat)
+      else
+        dump_data = YAML.dump(result)
+      end
+
+      open(filename, "w") do |f|
+        f.puts dump_data
+      end
+    end
+
+    private
+    def parse_args
       opts = Trollop::options do
         opt :retweets, "Include retweets to output"
         opt :json, "Use JSON"
       end
-
       screen_name = ARGV.first
-      result = collect(screen_name, include_retweets: opts[:retweets])
+      [screen_name, opts]
+    end
 
-      result.map!(&:to_h)
-      ext = opts[:json] ? ".json" : ".yml"
-      dump_data =
-        if opts[:json]
-          Oj.dump(result, mode: :compat)
-        else
-          YAML.dump(result)
-        end
-
-      filename = "alltweets_#{screen_name}#{ext}"
-      puts "Saving #{screen_name}'s all tweets to #{filename}"
-      open("alltweets_#{screen_name}#{ext}", "w") do |f|
-        f.puts dump_data
-      end
+    def filename
+      ext = @opts[:json] ? ".json" : ".yml"
+      "alltweets_#{@screen_name}#{ext}"
     end
   end
 end
